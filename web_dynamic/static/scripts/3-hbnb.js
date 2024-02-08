85e72f90e73eba1e73f790c338faa0bf3f10cf6e
Erkenoss <xavier.bertin6@gmail.com>
2024-02-08 12:17:12 +0100

    add task 4-

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,87 +1,52 @@
-fetch('http://0.0.0.0:5001/api/v1/status/')
-  .then(data => {
-      const statusApi = document.getElementById('api_status');
-      if (data.status === 'OK') {
-          statusApi.classList.add('available');
-      } else {
-          statusApi.classList.remove('available');
-      }
-  })
-  .catch(error =>  {
-      console.log(error);
-  });
-
-fetch('http://0.0.0.0:5001/api/v1/places_search/', {
-  method: 'POST',
-  headers: {
-      'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({})
-})
-  .then(response => response.json())
-  .then(data => {
-      data.forEach(place => {
-          const placedesc = place.description.replace(/Owner:.*?\n/, '');
-
-          const article = document.createElement('article');
-          article.innerHTML = `
-              <h2>${place.name}</h2>
-              <p>${placedesc}</p>
-          `
-          placeSection.appendChild(article);
-      })
-  })
-  .catch(error => {
-      console.error('Error:', error);
-  });
-
-
 $(document).ready(function () {
-  let amenitiesChecked = {};
+  const amenitiesSelected = {};
 
-  $('input[type=checkbox]').change(function () {
-    let amenityID = $(this).closest('li').data('id');
-    let amenityName = $(this).closest('li').data('name');
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).closest('li').data('id');
+    const amenityName = $(this).closest('li').data('name');
 
     if ($(this).is(':checked')) {
-      amenitiesChecked[amenityID] = amenityName;
+      amenitiesSelected[amenityId] = amenityName;
     } else {
-      delete amenitiesChecked[amenityID];
+      delete amenitiesSelected[amenityId];
     }
 
-    updateAmenitiesList();
-
-    $.ajax({
-      type: 'POST',
-      url: 'http://localhost:5001/api/v1/places_search',
-      data: '{}',
-      dataType: 'json',
-      contentType: 'application/json',
-      success: function (data) {
-        $('SECTION.places').append(data.map(place => {
-          return `<article>
-                      <div class="title_box">
-                        <h2>${place.name}</h2>
-                        <div class="price_by_night">${place.price_by_night}</div>
-                      </div>
-                      <div class="information">
-                        <div class="max_guest">${place.max_guest} Guests</div>
-                        <div class="number_rooms">${place.number_rooms} Bedrooms</div>
-                        <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
-                      </div>
-                      <div class="description">
-                        ${place.description}
-                      </div>
-                    </article>`;
-        }));
-      }
-    });
-
+    const amenitiesList = Object.values(amenitiesSelected).join(', ');
+    $('div.amenities h4').text(amenitiesList);
   });
 
-  function updateAmenitiesList() {
-    let h4Tag = $('div.amenities h4');
-    const amenList = Object.values(amenitiesChecked).join(', ');
-    h4Tag.text(amenList);
-  }
+  $.get('http://localhost:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:5001/api/v1/places_search',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      $('SECTION.places').append(data.map(place => {
+        return `<article>
+                    <div class="title_box">
+                      <h2>${place.name}</h2>
+                      <div class="price_by_night">${place.price_by_night}</div>
+                    </div>
+                    <div class="information">
+                      <div class="max_guest">${place.max_guest} Guests</div>
+                      <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                      <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                    </div>
+                    <div class="description">
+                      ${place.description}
+                    </div>
+                  </article>`;
+      }));
+    }
+  });
 });
+
